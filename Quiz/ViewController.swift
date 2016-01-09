59a1a4649d3baa04ea4a3338b558355f000de0ce
高橋詩穂 <Shiho@takahashiutaho-no-macbook-pro.local>
2016-01-09 15:52:05 +0900

    Initial Commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by 高橋詩穂 on 2016/01/09.
+//  Copyright © 2016年 Shiho Takahashi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
